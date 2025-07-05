6d3f78d037b71e8e4848ab74e1e4e85ed549cf93
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-07-05 16:18:51 -0300

    feature: Criação inicial da activivity de sessão

---
+++ app/src/main/java/io/github/luishenriqueaguiar/ui/activities/focus/FocusSessionActivity.kt
@@ -0,0 +1,12 @@
+package io.github.luishenriqueaguiar.ui.activities.focus
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.github.luishenriqueaguiar.R
+
+class FocusSessionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_focus_session)
+    }
+}
