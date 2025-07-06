2a8edcb52838985efd6beb5bd54489103a605464
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-07-06 13:46:20 -0300

    feature: Criação do layout da página de criação de sessão e implementação da entidade de sessão

--- app/src/main/java/io/github/luishenriqueaguiar/domain/model/Session.kt
+++ app/src/main/java/io/github/luishenriqueaguiar/domain/model/Session.kt
@@ -1,4 +1,16 @@
 package io.github.luishenriqueaguiar.domain.model
 
-class Session {
-}
+import java.util.Date
+
+data class Session (
+    val id: String,
+    val userId: String,
+    val name: String,
+    val plannedStudyDurationInMinutes: Int,
+    val plannedBreakDurationInMinutes: Int,
+    val status: SessionStatus,
+    val startTime: Date?,
+    val endTime: Date?,
+    val actualStudyDurationInSeconds: Long,
+    val totalBreakDurationInSeconds: Long
+)
---
+++ app/src/main/java/io/github/luishenriqueaguiar/domain/model/SessionStatus.kt
@@ -0,0 +1,7 @@
+package io.github.luishenriqueaguiar.domain.model
+
+enum class SessionStatus {
+    COMPLETED,
+    ABANDONED,
+    IN_PROGRESS
+}
